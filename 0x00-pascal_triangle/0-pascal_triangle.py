605e9ceb5af91625773684d1a449769cbafbf8bd
Annah Nokutheza Mwanza <annahmkhwanazi18@gmail.com>
2023-07-02 16:48:47 +0200

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -16,18 +16,17 @@ def pascal_triangle(n):
     Raises:
         None.
     """
-    
+
     if n <= 0:
         return []
 
-    triangle = [[1]]
-    while len(triangle) < n:
-        prev_row = triangle[-1]
-        new_row = [1]
+    triangle = [[1]]  # Initialize the triangle with the first row
+    while len(triangle) < n:  # Loop until the desired number of rows is reached
+        prev_row = triangle[-1]  # Get the previous row
+        new_row = [1]  # The new row always starts with 1
         for i in range(len(prev_row) - 1):
-            new_row.append(prev_row[i] + prev_row[i + 1])
-        new_row.append(1)
-        triangle.append(new_row)
+            new_row.append(prev_row[i] + prev_row[i + 1])  # Compute the values for the new row
+        new_row.append(1)  # The new row always ends with 1
+        triangle.append(new_row)  # Add the new row to the triangle
 
     return triangle
-
