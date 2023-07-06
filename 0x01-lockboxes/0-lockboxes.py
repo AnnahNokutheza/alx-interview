d338667a596c141bef4d20b0e061e31abd15de2a
Annah Nokutheza Mwanza <annahmkhwanazi18@gmail.com>
2023-07-06 10:47:24 +0200

    Create 0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+def canUnlockAll(boxes):
+    num_boxes = len(boxes)
+    visited = [False] * num_boxes  # Keep track of visited boxes
+    visited[0] = True  # Mark the first box as visited
+    queue = deque([0])  # Start BFS from the first box
+
+    while queue:
+        current_box = queue.popleft()
+        keys = boxes[current_box]  # Get the keys in the current box
+
+        for key in keys:
+            if key >= 0 and key < num_boxes and not visited[key]:
+                visited[key] = True  # Mark the box as visited
+                queue.append(key)  # Add the box to the queue for further exploration
+
+    # Check if all boxes have been visited
+    return all(visited)
